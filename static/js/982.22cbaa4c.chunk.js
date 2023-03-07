9c5fd2df558232dc4eb6bcf45dc4955a81ed76e3
Ji-hoon Lee <anteater1056@gmail.com>
2023-03-07 17:27:00 +0900

    Updates

---
+++ static/js/982.22cbaa4c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknamu_soup=self.webpackChunknamu_soup||[]).push([[982],{6982:function(n,u,a){a.r(u);var s=a(184);u.default=function(){var n="",u=Math.random();return n=u<=.33?"\uc798\ubabb \ub4e4\uc5b4\uc624\uc168\uc2b5\ub2c8\ub2e4.":u<=.66?"\uc774 \uae38\uc774 \uc544\ub2d9\ub2c8\ub2e4.":"\ud398\uc774\uc9c0\uac00 \uc874\uc7ac\ud558\uc9c0 \uc54a\uc2b5\ub2c8\ub2e4.",(0,s.jsxs)("div",{className:"not-found-page",children:[n,(0,s.jsx)("br",{}),"404 Page Not Found"]})}}}]);
+//# sourceMappingURL=982.22cbaa4c.chunk.js.map
