55a1fba3bbf42812bf219b5f0a83ee56fbe256c4
Ji-hoon Lee <anteater1056@gmail.com>
2023-07-24 11:06:34 +0900

    class => className

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -2,15 +2,15 @@ import ghLogo from "../assets/github-mark.svg";
 
 function Footer() {
   return (
-    <footer class="footer">
-      <div class="footer-gh-logo">
+    <footer className="footer">
+      <div className="footer-gh-logo">
         <a href="https://github.com/anteater333/namu-soup">
           <img src={ghLogo} alt="github repo" />
         </a>
       </div>
-      <div class="footer-copyright">
+      <div className="footer-copyright">
         Copyright© 2023.
-        <a class="text-dark" href="https://github.com/anteater333">
+        <a className="text-dark" href="https://github.com/anteater333">
           {" "}
           anteater333
         </a>{" "}
